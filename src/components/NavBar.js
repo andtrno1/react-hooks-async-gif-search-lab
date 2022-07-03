974f87a6b4c95e5a7cc047d1b9e4a3bfc47851aa
andtrno1 <91647620+andtrno1@users.noreply.github.com>
2022-07-03 15:07:18 -0400

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar({ color, title }) {
   const colors = {
     black: "navbar-inverse",
-    white: "navbar-default",
+    white: "navbar-default"
   };
 
   return (
